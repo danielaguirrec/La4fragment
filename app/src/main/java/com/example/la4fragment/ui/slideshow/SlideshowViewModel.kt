a1d3ea5d6c5460c29e0f4513f61c4371237f26a4
danielaguirrec <danielaac2020@gmail.com>
2019-09-16 13:58:10 -0500

    Initial commit

---
+++ app/src/main/java/com/example/la4fragment/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.la4fragment.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = ""
+    }
+    val text: LiveData<String> = _text
+}
